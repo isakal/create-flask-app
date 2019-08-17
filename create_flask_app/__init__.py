b2d120944b90f592cb5f2af353545458b0f2389a
isakal <sakyYT@gmail.com>
2019-08-17 18:22:57 +0200

    Trying to fix the problem with activating the venv

--- create_flask_app/__init__.py
+++ create_flask_app/__init__.py
@@ -9,6 +9,7 @@ import time
 NEWLINE = '\n'
 init()
 
+
 @click.command()
 @click.argument('project_name')
 @click.option('--api', is_flag=True, default=False, help='Will create Flask app that resembles API.')
@@ -23,17 +24,19 @@ def create_project(project_name, api, spa, nodb):
 
     try:
         create_folder(project_name)
-        #create_init()
+        # create_init()
     except FileExistsError:
         click.echo(NEWLINE)
-        click.echo(Fore.RED + 'That directory already exists. Please check your project name and try again.')
+        click.echo(
+            Fore.RED + 'That directory already exists. Please check your project name and try again.')
         click.echo(Style.RESET_ALL)
         sys.exit(1)
 
     #ante = click.prompt('unesite ime: ')
-    #click.echo(ante)
+    # click.echo(ante)
 
-    click.echo(NEWLINE + 'Creating a new Flask app in ' + Fore.GREEN + f'~/{project_name}.')
+    click.echo(NEWLINE + 'Creating a new Flask app in ' +
+               Fore.GREEN + f'~/{project_name}.')
     click.echo(Style.RESET_ALL)
 
     create_venv(f'./{project_name}/venv/')
@@ -41,4 +44,4 @@ def create_project(project_name, api, spa, nodb):
     if sys.platform == 'win32':
         activate_venv(f'./{project_name}/venv/Scripts')
     elif sys.platform == 'linux':
-        activate_venv(f'./{project_name}/venv/bin')
+        activate_venv(project_name)
--- create_flask_app/utils.py
+++ create_flask_app/utils.py
@@ -8,32 +8,34 @@ import os
 
 """Long running functions"""
 
+
 @animation.wait('spinner')
 def create_venv(dir: str):
     virtualenv = venv.EnvBuilder(with_pip=True)
     virtualenv.create(f"./{dir}")
 
 
-
 """helper functions"""
 
+
 def create_file(file_name: str):
-    file = open(file_name, 'w').close()
+    open(file_name, 'w').close()
 
 
 def create_folder(folder_name: str):
     os.mkdir(folder_name)
 
 
-def create_init(directory: str='./'):
-    file = open(directory + '__init__.py', 'w').close()
+def create_init(directory: str = './'):
+    open(directory + '__init__.py', 'w').close()
 
 
 def activate_venv(project_name: str):
     #shell = detect_shell()
     if sys.platform == "win32":
-        subprocess.Popen(f"{project_name}\\venv\\Scripts\\activate.bat", shell=True)
+        subprocess.Popen(
+            f"{project_name}\\venv\\Scripts\\activate.bat", shell=True)
     elif sys.platform == "linux":
-        subprocess.Popen(f"source {project_name}/venv/bin/activate", shell=True)
-        subprocess.Popen("echo $SHELL", shell=True)
-    #TODO figure out how to execute shell commands without using Popen/system
+        subprocess.Popen(
+            f". {project_name}/venv/bin/activate", shell=True)
+    # FIXME: figure out how to execute shell commands within current shell
