5a02109e12ce71a4a6177a9f0f546e0f368d3faa
isakal <sakyYT@gmail.com>
2019-11-21 22:04:36 +0100

    Fix CORS issuse and make the button work - spa completed [x]

--- create_flask_app/spa/app/static/js/button.js
+++ create_flask_app/spa/app/static/js/button.js
@@ -1,9 +1,9 @@
 async function get_num() {
-    const host = "http://" + window.location.hostname;
+    const host = "http://" + window.location.host;
 
     // making a post request to the server
     const response = await fetch(host, {
-        method : "POST"
+        method: "POST"
     });
 
     // getting the json response
@@ -12,11 +12,11 @@ async function get_num() {
     // put number in the html
     const number = json_data.random_num;
 
-    if (document.getElementsByTagName("p"))
-        document.getElementsByTagName("p").parentNode.removeChild(document.getElementsByTagName("p"));
-    var p = document.createElement("p");
-    if (p.innerHTML){
-        p.innerHTML = number;
-        }
-   
+    const p_find = document.getElementsByTagName("P");
+    const p = p_find.length > 0 ? p_find[0] : document.createElement("p");
+
+    document.body.appendChild(p);
+
+    p.innerText = number;
+
 }
