0e30d3f2ba81b20fbbec93c9ff185071bdaf7653
isakal <sakyYT@gmail.com>
2019-11-25 20:37:23 +0100

    Remove CORS from default boilerplate since it's redundant code

--- create_flask_app/default/app/__init__.py
+++ create_flask_app/default/app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from app.config import TestingConfig, DevelopmentConfig, ProductionConfig
 import os
-from flask_cors import CORS
 # blueprints
 from app.errors.handlers import errors
 from app.home.routes import home
 
 
 app = Flask(__name__)
-CORS(app)
 app.config.from_object(DevelopmentConfig)
 
 app.register_blueprint(errors)
